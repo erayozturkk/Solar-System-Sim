38e994c3ac183ddc628f2a88af39e6223fac6c7b
erayozturkk <erayozturk@sabanciuniv.edu>
2024-12-16 14:09:13 +0300

    init commit

---
+++ src/sceneNode.js
@@ -0,0 +1,53 @@
+/**
+ * @class SceneNode
+ * @desc A SceneNode is a node in the scene graph.
+ * @property {MeshDrawer} meshDrawer - The MeshDrawer object to draw
+ * @property {TRS} trs - The TRS object to transform the MeshDrawer
+ * @property {SceneNode} parent - The parent node
+ * @property {Array} children - The children nodes
+ */
+
+class SceneNode {
+    constructor(meshDrawer, trs, parent = null) {
+        this.meshDrawer = meshDrawer;
+        this.trs = trs;
+        this.parent = parent;
+        this.children = [];
+
+        if (parent) {
+            this.parent.__addChild(this);
+        }
+    }
+
+    __addChild(node) {
+        this.children.push(node);
+    }
+
+    draw(mvp, modelView, normalMatrix, modelMatrix) {
+        /**
+         * @Task1 : Implement the draw function for the SceneNode class.
+         */
+    
+        // Compute the node's transformation matrix
+        const nodeTransform = this.trs.getTransformationMatrix();
+    
+        // Update the transformation matrices
+        const transformedModel = MatrixMult(modelMatrix, nodeTransform);
+        const transformedModelView = MatrixMult(modelView, nodeTransform);
+        const transformedMvp = MatrixMult(mvp, nodeTransform);
+    
+        // Use the provided normalMatrix or recompute it dynamically
+        const transformedNormals = normalMatrix ? MatrixMult(normalMatrix, nodeTransform) : getNormalMatrix(transformedModelView);
+    
+        // Draw the MeshDrawer
+        if (this.meshDrawer) {
+            this.meshDrawer.draw(transformedMvp, transformedModelView, transformedNormals, transformedModel);
+        }
+    
+        // Recursively call draw on all children
+        for (const child of this.children) {
+            child.draw(transformedMvp, transformedModelView, transformedNormals, transformedModel);
+        }
+    }
+    
+}
